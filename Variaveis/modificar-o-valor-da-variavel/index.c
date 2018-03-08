f49a5ef61d09f0d75d261f509c931e797373b59b
William Trindade <williamtrindade777@gmail.com>
2018-03-08 16:05:59 -0300

    Create index.c

---
+++ Variaveis/modificar-o-valor-da-variavel/index.c
@@ -0,0 +1,10 @@
+//bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+main(){
+  //declarar uma variavel
+	int x = 10;
+  	x=5;
+	printf("Estamos criando um programa de computador\n");
+	system("pause");
+}
