5e2bfdd9d47fd50531be92cfc9b2c9baadf83356
William Trindade <williamtrindade777@gmail.com>
2018-03-12 15:40:16 -0300

    Create index.c

---
+++ Operadores/matematicos/index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+	int v1, v2;
+	printf("Informe o valor de 1: ");
+	scanf("%d", &v1);
+	printf("Informe o valor 2: ");
+	scanf ("%d", &v2);
+	int soma = v1 + v2;
+	printf("\n O resultado da soma foi : %d \n" , soma);
+	system("pause");
+}
