a84a5d2d72af9b7a474ce0928f38b85f6e93e243
William Trindade <williamtrindade777@gmail.com>
2018-03-12 15:22:43 -0300

    Create index.c

---
+++ Entrada de Dados/scanf/index.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+	int total;
+	printf("Iforme o total de aluno : ");
+	scanf("%d", &total);
+	printf("\n O valor digitado foi: %d \n", total);
+	system ("pause");
+}
