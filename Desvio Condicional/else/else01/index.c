6b11cebc7f76ab0ce919212de6fdbfd5120800ab
William Trindade <williamtrindade777@gmail.com>
2018-03-15 14:47:22 -0300

    Create index.c

---
+++ Desvio Condicional/else/else01/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(){
+	int v1;
+	printf("Digite o valor \n");
+	scanf("%d", &v1);
+	if(v1 > 0){
+		printf("POSITIVO \n");
+	}else{
+		printf("NEGATIVO \n");
+	}
+	system("pause");
+}
