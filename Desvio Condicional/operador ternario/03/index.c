3ec6c37a14d9d61667dbdbf56afe17301955a9fb
William Trindade <williamtrindade777@gmail.com>
2018-03-22 14:25:35 -0300

    Create index.c

---
+++ Desvio Condicional/operador ternario/03/index.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+main(){
+	float salario;
+	printf("Qual seu salario \n");
+	scanf("%f", &salario);
+	salario <= 2000 ? printf("Seu salario e: %.2f \n", salario = salario + 500) :  printf("Seu salario e: %.2f \n", salario = salario + 200 );
+}
