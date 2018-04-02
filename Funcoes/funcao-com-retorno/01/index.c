6d93092566974cb913812b8ad974ea42a40a8b50
William Trindade <williamtrindade777@gmail.com>
2018-04-02 14:30:40 -0300

    Create index.c

---
+++ Funcoes/funcao-com-retorno/01/index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(){
+	int x = 50, y=100;
+	int s = soma(x, y);
+	printf("%d \n", s);	
+}
+ int soma(int a, int b){
+	a = 200;
+	int resultado = a + b;
+	return resultado;
+}
