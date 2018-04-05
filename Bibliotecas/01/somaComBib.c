0a571979a6d2a085d2a2aed8979b789a3d3f8f2f
William Trindade <williamtrindade777@gmail.com>
2018-04-05 15:13:19 -0300

    Add files via upload

---
+++ Bibliotecas/01/somaComBib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "teste.h"
+
+main(){
+	soma();
+	system("pause");
+}
---
+++ Bibliotecas/01/teste.h
@@ -0,0 +1,10 @@
+void soma(){
+	int x = 10;
+	int y = 20; 
+	printf("%d \n", x+y );
+}
+void subtrai(){
+	int x = 10;
+	int y = 20;
+	printf("%d \n", x-y);
+}
